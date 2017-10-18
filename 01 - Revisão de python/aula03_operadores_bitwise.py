08b6a24176b983fedce24ca231bd2ed6fb3b3239
JPdeAzevedoLima <jpdeazevedolima@gmail.com>
2017-10-17 21:18:11 -0300

    Arquivos de aula

---
+++ 01 - Revisão de python/aula03_operadores_bitwise.py
@@ -0,0 +1,10 @@
+x=1
+x<<2
+print(x)
+print(x | 2)
+print(x & 1)
+y = 0b1000
+print(y)
+y=y>>3
+#Y=0b0001
+print(y)
---
+++ 01 - Revisão de python/aula05_slice.py
@@ -0,0 +1,18 @@
+x='python'
+print(x[0]) #retorno 'p'
+print(x[-1]) #retorno 'n'
+print(x[1:4]) #retorno 'yth'
+
+print(x[2:])
+print(x[:3])
+
+s='abcdefghijklmnop'
+print(s[1:10:2])
+#retorno 'bdfhj'
+
+print(s[::2])
+#retorno 'acegikmo'
+
+p = 'alo'
+print(p[::-1])
+#retorno 'ola'
