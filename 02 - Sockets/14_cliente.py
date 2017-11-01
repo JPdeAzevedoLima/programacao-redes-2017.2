0f56f2a64ba0b884f7469cbb8f9fddaa0354dd49
JPdeAzevedoLima <jpdeazevedolima@gmail.com>
2017-11-01 18:01:37 -0300

    Alteração do código

--- 02 - Sockets/14_cliente.py
+++ 02 - Sockets/14_cliente.py
@@ -2,14 +2,14 @@
 
 import socket
 
-HOST = socket.gethostbyname('localhost')
+HOST = '10.25.3.174'
 PORT = 2000
 
 tcp_client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 tcp_client_socket.connect((HOST,PORT))
-message = "Hello, World!"
-byte_msg = message.incode('utf-8')
+message = "João Paulo de Azevedo Lima"
+byte_msg = message.encode('utf-8')
 tcp_client_socket.send(byte_msg)
 data = tcp_client_socket.recv(1024)
 tcp_client_socket.close
