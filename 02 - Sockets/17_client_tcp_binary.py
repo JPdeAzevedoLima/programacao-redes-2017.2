6c73f58ac577c34433fed57e7e1cc5986e00c081
JPdeAzevedoLima <jpdeazevedolima@gmail.com>
2017-11-27 17:55:32 -0300

    Alteração Cliente

---
+++ 02 - Sockets/17_client_tcp_binary.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.6
+
+import socket
+
+TCP_IP = '10.25.1.140'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+with open('arquivo_recebido.txt','wb') as f:
+    print("Arquivo foi aberto")
+    while True:
+        data = s.recv(BUFFER_SIZE)
+        print('data = %s',(data))
+        if not data:
+            f.close()
+            print("Arquivo fechado")
+            break
+        f.write(data)
+
+print("Arquivo transferido com sucesso")
+s.close()
+print("Conexão encerrada")
